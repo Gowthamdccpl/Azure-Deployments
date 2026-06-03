1f8996c821d45b10bf13dd1a3cf75b777d61b9ce
Gowthamkumarks17 <gowthmks1010@gmail.com>
2026-06-03 14:45:51 +0530

    changed Terraform provider version issue.

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -8,6 +8,6 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_share" "fileshare" {
   name               = var.file_share_name
-  storage_account_id = azurerm_storage_account.storage.id
+  storage_account_name = azurerm_storage_account.storage.name
   quota              = 100
 }
