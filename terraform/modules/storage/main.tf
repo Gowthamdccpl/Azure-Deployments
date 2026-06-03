919f2ffde1469acac343a53e058f8cfc8a0648d8
Gowthamkumarks17 <gowthmks1010@gmail.com>
2026-06-03 14:49:58 +0530

    fix terraform formatting

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_share" "fileshare" {
-  name               = var.file_share_name
+  name                 = var.file_share_name
   storage_account_name = azurerm_storage_account.storage.name
-  quota              = 100
+  quota                = 100
 }
